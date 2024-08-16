478e84782b100d71d8ef9b3f2c5582c0754ad8ea
Nicholas Miller <nm@era.dk>
2024-08-16 05:36:01 +0300

    init module with menu and system.

---
+++ registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Copyright © Era. All rights reserved.
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Era_Base',
+    __DIR__
+);
